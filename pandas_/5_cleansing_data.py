c1e130dd720368ab76385982d632c7a5f5c055df
ankitwaskle <ankitwaskle143@gmail.com>
2024-10-06 11:21:28 +0530

    matploilib

--- pandas_/5_cleansing_data.py
+++ pandas_/5_cleansing_data.py
@@ -1,7 +1,62 @@
+# # # # # # #import pandas as pd
+# # # # # # #df = pd.read_csv('..//csv//Book1.csv')
+# # # # # # #new_df = df.dropna()
+# # # # # # #print(new_df.to_string())
+# # # # # # #
+# # # # # # # import pandas as pd
+# # # # # # #
+# # # # # # # df = pd.read_csv('data.csv')
+# # # # # # #
+# # # # # # # df.dropna(inplace = True)
+# # # # # # #
+# # # # # # # print(df.to_string())
+# # # # # #
+# # # # # # import pandas as pd
+# # # # # #
+# # # # # # df = pd.read_csv('data.csv')
+# # # # # #
+# # # # # # df.fillna(130, inplace = True)
+# # # # # # print(df.to_string())
+# # # # # import pandas as pd
+# # # # #
+# # # # # df = pd.read_csv('data.csv')
+# # # # #
+# # # # # df["Calories"].fillna(130, inplace = True)
+# # # # #
+# # # # # print(df.to_string())
+# # # # import pandas as pd
+# # # #
+# # # # df = pd.read_csv('data.csv')
+# # # #
+# # # # x = df["Calories"].mean()
+# # # #
+# # # # df["Calories"].fillna(x, inplace = True)
+# # #
+# # import pandas as pd
+# #
+# # df = pd.read_csv('data.csv')
+# #
+# # x = df["Calories"].median()
+# #
+# # df["Calories"].fillna(x, inplace = True)
+# #
+# # print(df.to_string())
+# #
+# # import pandas as pd
+# #
+# # df = pd.read_csv('data.csv')
+# #
+# # x = df["Calories"].mode()[0]
+# #
+# # df["Calories"].fillna(x, inplace = True)
+#
+#
 import pandas as pd
 
-df = pd.read_csv('..//csv//Book1.csv')
+df = pd.read_csv('data.csv')
 
-new_df = df.dropna()
+df['Date'] = pd.to_datetime(df['Date'])
+df.dropna(subset=['Date'], inplace = True)
+print(df.to_string())
 
-print(new_df.to_string())
+df.loc[7, 'Duration'] = 45
